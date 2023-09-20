2ab3326efd08cefaa18ebc0623d1b36d895de764
oxahex <oxahex@gmail.com>
2023-09-20 18:46:06 +0900

    test: 계좌 사용 컨트롤러 테스트 코드 작성

---
+++ src/test/java/com/oxahex/accountapi/controller/TransactionControllerTest.java
@@ -0,0 +1,65 @@
+package com.oxahex.accountapi.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oxahex.accountapi.dto.TransactionDto;
+import com.oxahex.accountapi.dto.UseBalance;
+import com.oxahex.accountapi.service.TransactionService;
+import com.oxahex.accountapi.type.TransactionResultType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TransactionController.class)
+class TransactionControllerTest {
+
+    @MockBean
+    private TransactionService transactionService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("잔액 사용 - 성공")
+    void useBalance() throws Exception {
+        // given: transaction 성공
+        given(transactionService.useBalance(anyLong(), anyString(), anyLong()))
+                .willReturn(TransactionDto.builder()
+                        .accountNumber("1234567890")
+                        .transactedAt(LocalDateTime.now())
+                        .amount(12345L)
+                        .transactionId("transactionId")
+                        .transactionResultType(TransactionResultType.S)
+                        .build());
+
+        // when
+        // then
+        mockMvc.perform(post("/transaction/use")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(
+                        new UseBalance.Request(1L, "1234567890", 10000L)
+                )))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountNumber").value("1234567890"))
+                .andExpect(jsonPath("$.transactionResult").value("S"))
+                .andExpect(jsonPath("$.transactionId").value("transactionId"))
+                .andExpect(jsonPath("$.amount").value(12345));
+    }
+}
