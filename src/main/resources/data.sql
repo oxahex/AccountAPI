408b4a693dcab7d46279d5e50f71a668c6718de1
oxahex <oxahex@gmail.com>
2023-09-20 12:09:54 +0900

    test: 계좌 생성 기능 http 요청 테스트

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into account_user(id, name, created_at, updated_at)
+values (1, '일영', now(), now());
+
+insert into account_user(id, name, created_at, updated_at)
+values (2, '영일', now(), now());
+
+insert into account_user(id, name, created_at, updated_at)
+values (3, '리코', now(), now());
