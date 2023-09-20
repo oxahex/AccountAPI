37f82c7aa73fe3c6cbe6f9fe522f198fbeb5838c
oxahex <oxahex@gmail.com>
2023-09-20 19:16:50 +0900

    test: 계좌 사용 서비스 테스트 코드 작성
    - 잔액 사용 성공
    - 유저가 없는 경우 실패
    - 계좌가 없는 경우 실패
    - 이미 해지 처리 된 계좌인 경우 실패
    - 거래 금액이 계좌 잔액보다 큰 경우 실패
    - 잔액 사용 실패 시 트랜잭션 저장 성공

---
+++ src/test/java/com/oxahex/accountapi/service/TransactionServiceTest.java
@@ -0,0 +1,238 @@
+package com.oxahex.accountapi.service;
+
+import com.oxahex.accountapi.domain.Account;
+import com.oxahex.accountapi.domain.AccountUser;
+import com.oxahex.accountapi.domain.Transaction;
+import com.oxahex.accountapi.dto.TransactionDto;
+import com.oxahex.accountapi.exception.AccountException;
+import com.oxahex.accountapi.repository.AccountRepository;
+import com.oxahex.accountapi.repository.AccountUserRepository;
+import com.oxahex.accountapi.repository.TransactionRepository;
+import com.oxahex.accountapi.type.AccountStatus;
+import com.oxahex.accountapi.type.ErrorCode;
+import com.oxahex.accountapi.type.TransactionResultType;
+import com.oxahex.accountapi.type.TransactionType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class TransactionServiceTest {
+    @Mock
+    private TransactionRepository transactionRepository;
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @Mock
+    private AccountUserRepository accountUserRepository;
+
+    @InjectMocks
+    private TransactionService transactionService;
+
+    @Test
+    @DisplayName("잔액 사용 - 성공")
+    void useBalance() {
+        // given: 유저와, 유저의 계좌(10000) 확인
+        AccountUser user = AccountUser.builder()
+                .id(1L).name("oxahex").build();
+        Account account = Account.builder()
+                .accountUser(user)
+                .accountStatus(AccountStatus.IN_USE)
+                .balance(10000L)
+                .accountNumber("1234567890").build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(account));
+
+        // given: 거래 결과가 저장됨
+        given(transactionRepository.save(any()))
+                .willReturn(Transaction.builder()
+                        .account(account)
+                        .transactionType(TransactionType.USE)
+                        .transactionResultType(TransactionResultType.S)
+                        .transactionId("transactionId")
+                        .transactedAt(LocalDateTime.now())
+                        .amount(1000L)
+                        .balanceSnapShot(1000L).build()
+                );
+
+        ArgumentCaptor<Transaction> captor = ArgumentCaptor.forClass(Transaction.class);
+
+        // when: 1000원 사용
+        TransactionDto transactionDto = transactionService.useBalance(
+                1L, "1234567890", 1000L
+        );
+
+        // then
+        verify(transactionRepository, times(1)).save(captor.capture());
+        // then: 계좌에 9000원 남음
+        assertEquals(9000L, captor.getValue().getBalanceSnapShot());
+        // then: 거래 결과 == 성공
+        assertEquals(TransactionResultType.S, transactionDto.getTransactionResultType());
+        // then: 거래 타입 == USE
+        assertEquals(TransactionType.USE, transactionDto.getTransactionType());
+        // then: 거래 금액 == 1000
+        assertEquals(1000L, captor.getValue().getAmount());
+    }
+
+    @Test
+    @DisplayName("잔액 사용 실패 - 유저 없음")
+    void useBalance_UserNotFound() {
+        // given: 유저가 없음
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        // when: 유저가 없는 경우(사용 시도 전에 Exception 처리)
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1234567890", 1000L));
+
+        // then: Exception USER_NOT_FOUND
+        assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("잔액 사용 실패 - 계좌 없음")
+    void useBalance_AccountNotFound() {
+        // given: 유저 있음
+        AccountUser user = AccountUser.builder().id(1L).name("oxahex").build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+
+        // given: 계좌 없음
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.empty());
+
+        // when: 계좌가 없는데 잔액 사용 시도
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1234567890", 1000L));
+
+        // then: Exception ACCOUNT_NOT_FOUND
+        assertEquals(ErrorCode.ACCOUNT_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("잔액 사용 실패 - 유저와 계좌 소유주가 다름")
+    void useBalance_UserUnMatch() {
+        // given: userA 유저가 잔액을 사용하려는데, 계좌의 소유주가 userB
+        AccountUser userA = AccountUser.builder().id(1L).name("userA").build();
+        AccountUser userB = AccountUser.builder().id(2L).name("userB").build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(userA));
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountUser(userB)
+                        .balance(0L)
+                        .accountNumber("1234567890").build()));
+
+        // when: 잔액 사용을 요청한 유저(userA)와 계좌 소유주(userB)가 다른데 잔액 사용 시도
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1234567890", 1000L));
+
+        // then: Exception USER_ACCOUNT_UN_MATCH
+        assertEquals(ErrorCode.USER_ACCOUNT_UN_MATCH, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("잔액 사용 실패 - 이미 해지 처리 된 계좌")
+    void useBalance_AlreadyUnRegisteredAccoun() {
+        // given: 계좌의 소유주와 유저 일치하나, 이미 해지 처리 된 계좌
+        AccountUser user = AccountUser.builder().id(1L).name("user").build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountUser(user)
+                        .balance(0L)
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("1234567890").build()));
+
+        // when: 이미 해지 처리 된 계좌의 잔액 사용 시도
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1234567890", 1000L));
+
+        // then: Exception ACCOUNT_ALREADY_UNREGISTERED
+        assertEquals(ErrorCode.ACCOUNT_ALREADY_UNREGISTERED, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("잔액 사용 실패 - 거래 금액이 잔액보다 큼")
+    void useBalance_AmountExceedBalance() {
+        // given: 거래 금액이 잔액보다 큼
+        AccountUser user = AccountUser.builder()
+                .id(1L).name("oxahex").build();
+        Account account = Account.builder()
+                .accountUser(user)
+                .accountStatus(AccountStatus.IN_USE)
+                .balance(100L)
+                .accountNumber("1234567890").build();
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(user));
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(account));
+
+        // when: 잔액이 100원인 계좌에서 10000원 사용 시도
+        AccountException exception = assertThrows(AccountException.class,
+                () -> transactionService.useBalance(1L, "1234567890", 10000L));
+
+        // then: Exception AMOUNT_EXCEED_BALANCE
+        assertEquals(ErrorCode.AMOUNT_EXCEED_BALANCE, exception.getErrorCode());
+        verify(transactionRepository, times(0)).save(any());
+    }
+
+    @Test
+    @DisplayName("잔액 사용 실패 트랜잭션 저장 - 성공")
+    void saveFailedUseTransaction() {
+        // given: 유저와, 유저의 계좌(10000) 확인
+        AccountUser user = AccountUser.builder()
+                .id(1L).name("oxahex").build();
+        Account account = Account.builder()
+                .accountUser(user)
+                .accountStatus(AccountStatus.IN_USE)
+                .balance(10000L)
+                .accountNumber("1234567890").build();
+        given(accountRepository.findByAccountNumber(anyString()))
+                .willReturn(Optional.of(account));
+
+        // given: 거래 결과가 저장됨
+        given(transactionRepository.save(any()))
+                .willReturn(Transaction.builder()
+                        .account(account)
+                        .transactionType(TransactionType.USE)
+                        .transactionResultType(TransactionResultType.S)
+                        .transactionId("transactionId")
+                        .transactedAt(LocalDateTime.now())
+                        .amount(1000L)
+                        .balanceSnapShot(1000L).build()
+                );
+
+        ArgumentCaptor<Transaction> captor = ArgumentCaptor.forClass(Transaction.class);
+
+        // when: 1000원 사용 시도 했으나 실패
+        transactionService.saveFailedUseTransaction("1234567890", 1000L);
+
+        // then
+        verify(transactionRepository, times(1)).save(captor.capture());
+        // then: 거래 금액 == 1000
+        assertEquals(1000L, captor.getValue().getAmount());
+        // then: 계좌에 10000원 남음(실패로 차감되지 않음)
+        assertEquals(10000L, captor.getValue().getBalanceSnapShot());
+        // then: 거래 결과 == 성공
+        assertEquals(TransactionResultType.F, captor.getValue().getTransactionResultType());
+    }
+}
