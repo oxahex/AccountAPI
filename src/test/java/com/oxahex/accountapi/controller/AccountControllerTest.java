aeffaa542946c4312a0342e69fd4f851d63ee0f5
oxahex <oxahex@gmail.com>
2023-09-20 13:32:17 +0900

    test: 계좌 생성 Controller Test 코드 작성
    - 계좌 생성

---
+++ src/test/java/com/oxahex/accountapi/controller/AccountControllerTest.java
@@ -0,0 +1,61 @@
+package com.oxahex.accountapi.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oxahex.accountapi.dto.AccountDto;
+import com.oxahex.accountapi.dto.CreateAccount;
+import com.oxahex.accountapi.service.AccountService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AccountController.class)
+class AccountControllerTest {
+
+    @MockBean
+    private AccountService accountService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("계좌 생성 성공")
+    void successCreateAccount() throws Exception {
+        // given: 계좌 생성 완료
+        given(accountService.createAccount(anyLong(), anyLong()))
+                .willReturn(AccountDto.builder()
+                        .userId(1L)
+                        .accountNumber("1234567890")
+                        .registeredAt(LocalDateTime.now())
+                        .unRegisteredAt(LocalDateTime.now())
+                        .build());
+        // when
+        // then: userId, accountNumber
+        mockMvc.perform(post("/account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(
+                        new CreateAccount.Request(1L, 100L)
+                )))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(1))
+                .andExpect(jsonPath("$.accountNumber").value("1234567890"))
+// TODO               .andExpect(jsonPath("$.registeredAt").value(LocalDateTime.now()))             시간값은?
+                .andDo(print());
+    }
+
+}
