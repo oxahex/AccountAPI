89dcf7962dee608cc6ac4f5312502c9df6504b0f
oxahex <oxahex@gmail.com>
2023-09-21 13:51:15 +0900

    refact: 일관성 있는 예외 처리

---
+++ src/main/java/com/oxahex/accountapi/dto/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.oxahex.accountapi.dto;
+
+import com.oxahex.accountapi.type.ErrorCode;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+    private ErrorCode errorCode;
+    private String errorMessage;
+}
