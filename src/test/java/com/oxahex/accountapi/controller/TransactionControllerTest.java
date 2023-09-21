7faffbc5b5e803149d1011110669b84552d526f9
oxahex <oxahex@gmail.com>
2023-09-21 13:11:39 +0900

    test: 거래 내역 조회 컨트롤러 테스트 코드 작성

--- src/test/java/com/oxahex/accountapi/controller/TransactionControllerTest.java
+++ src/test/java/com/oxahex/accountapi/controller/TransactionControllerTest.java
@@ -1,12 +1,14 @@
 package com.oxahex.accountapi.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oxahex.accountapi.domain.Transaction;
+import com.oxahex.accountapi.dto.AccountDto;
 import com.oxahex.accountapi.dto.CancelBalance;
 import com.oxahex.accountapi.dto.TransactionDto;
 import com.oxahex.accountapi.dto.UseBalance;
 import com.oxahex.accountapi.service.TransactionService;
 import com.oxahex.accountapi.type.TransactionResultType;
+import com.oxahex.accountapi.type.TransactionType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +18,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
+import static com.oxahex.accountapi.type.TransactionResultType.S;
+import static com.oxahex.accountapi.type.TransactionType.USE;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -91,5 +98,31 @@ class TransactionControllerTest {
                 .andExpect(jsonPath("$.transactionResult").value("S"))
                 .andExpect(jsonPath("$.transactionId").value("transactionId"))
                 .andExpect(jsonPath("$.amount").value(54321));
+    }
+
+    @Test
+    @DisplayName("거래 내역 조회 - 성공")
+    void getQueryTransaction() throws Exception {
+        // given: 거래
+
+        given(transactionService.queryTransaction(anyString()))
+                .willReturn(TransactionDto.builder()
+                        .accountNumber("1234567890")
+                        .transactionType(USE)
+                        .transactedAt(LocalDateTime.now())
+                        .amount(12345L)
+                        .transactionId("transactionId")
+                        .transactionResultType(S).build());
+
+        // when
+        // then
+        mockMvc.perform(get("/transaction/12345"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accountNumber").value("1234567890"))
+                .andExpect(jsonPath("$.transactionType").value("USE"))
+                .andExpect(jsonPath("$.transactionResult").value("S"))
+                .andExpect(jsonPath("$.transactionId").value("transactionId"))
+                .andExpect(jsonPath("$.amount").value(12345));
     }
 }
